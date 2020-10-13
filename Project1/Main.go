6cf19b85d42a48116978b597bfd07b9100333e34
TKzizo <ia_takouche@esi.dz>
2020-10-13 16:40:20 +0100

    Project1

---
+++ Project1/Main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var esi string = "Ecole national Superieur informatique"
+	polytech := "Ecole National Superieur Polytechnique"
+	fmt.Printf("%v ,%T \n", esi, esi)
+	fmt.Printf("%v ,%T \n", polytech, polytech)
+}
